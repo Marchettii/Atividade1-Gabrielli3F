b119152150907b305e12324dbc0f127146c45824
Marchettii <103433358+Marchettii@users.noreply.github.com>
2024-08-05 22:53:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Gabrielli Marchetti";
+const idade = 17;
+const altura = 1.63;
+const massa = 60
+const cidade = "São José Dos Pinhais"
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
