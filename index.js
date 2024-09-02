4f3f95ef4718d10783aea2bdabca3063b2af4e93
Marchettii <103433358+Marchettii@users.noreply.github.com>
2024-09-02 22:55:31 +0000

    02/09

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const meuNome = "Gabrielli Marchetti";
-const idade = 17;
-const altura = 1.63;
-const massa = 60
-const cidade = "São José Dos Pinhais"
+let idade = 17;
+const anoNascimento = 2007;
+let anoAtual = 2024;
+const nomeCompleto = "Gabrielli Marchetti";
 
-console.log(meuNome);
-console.log(idade);
-console.log(altura);
-console.log(massa);
-console.log(cidade);
+console.log("Meu nome é " + Gabrielli Marchetti + " nascido no ano de " + 2007 + " e no ano atual " + 2025 + " completo " + 18 + " anos ");
+
+anoAtual = anoAtual + 1;
+console.log(anoAtual)
+idade = idade  + 1;
+console.log(idade)
+
+
+//Agência de viagens Vancouver, Seul, Abu Dhabi, Taiwan, Moscou
+
+const listaDeDestinos = new Array(
+  "Vancouver",
+  "Seul",
+  "Abu Dhabi",
+  "Taiwan",
+  "Moscou"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[4]);
+
+listaDeDestinos.push("Canada");
+listaDeDestinos.push("Bonito");
+listaDeDestinos.push("Orlando");
+listaDeDestinos.push("Gramado");
+
+console.log(listaDeDestinos);
